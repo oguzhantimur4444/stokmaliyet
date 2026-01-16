675600015b9cf388812293daf9e55abbfba03439
oguzhantimur4444 <oguzhantimur4444@gmail.com>
2026-01-16 13:22:55 +0300

    first commit

---
+++ src/main/java/com/inonu/stokmaliyet/StokmaliyetApplication.java
@@ -0,0 +1,13 @@
+package com.inonu.stokmaliyet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StokmaliyetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StokmaliyetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inonu/stokmaliyet/StokmaliyetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inonu.stokmaliyet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StokmaliyetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
